ea3c369bbf4e7f5c6e2ced03ac003d3ada69715d
Axiaobo <Axiaobo7788@outlook.com>
2026-04-30 21:04:56 +0800

    fix: tabbar

--- uni/pages/device/index.vue
+++ uni/pages/device/index.vue
@@ -1,8 +1,7 @@
 <template>
   <view class="device-page">
     <web-view src="http://192.168.0.71:8123" :update-title="false"></web-view>
-    
-    <!-- 使用 cover-view 实现自定义底栏，确保层级在 web-view 之上 -->
+
     <cover-view class="custom-tabbar">
       <cover-view class="tab-item" @click="switchTab('/pages/index/index')">
         <cover-view class="tab-icon">🏠</cover-view>
@@ -31,7 +30,6 @@
 <script>
 export default {
   onShow() {
-    // 隐藏系统 TabBar，使用自定义 cover-view 底栏
     uni.hideTabBar()
   },
   methods: {
@@ -47,17 +45,19 @@ export default {
   width: 100%;
   height: 100vh;
 }
+
 .custom-tabbar {
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
-  height: 50px;
+  height: 100rpx;
   background-color: #ffffff;
   border-top: 1px solid #e5e5e5;
   display: flex;
   z-index: 999;
 }
+
 .tab-item {
   flex: 1;
   display: flex;
@@ -65,14 +65,17 @@ export default {
   align-items: center;
   justify-content: center;
 }
+
 .tab-icon {
-  font-size: 20px;
-  margin-bottom: 2px;
+  font-size: 40rpx;
+  margin-bottom: 4rpx;
 }
+
 .tab-text {
-  font-size: 10px;
+  font-size: 22rpx;
   color: #999999;
 }
+
 .tab-item.active .tab-text {
   color: #007AFF;
 }
