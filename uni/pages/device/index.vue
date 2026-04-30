5b4a3dc59c5e6429c3177a369533d48f718259e6
Axiaobo <Axiaobo7788@outlook.com>
2026-04-30 19:37:15 +0800

    opt: fix 	modified:   uni/pages.json 	modified:   uni/pages/device/index.vue

--- uni/pages/device/index.vue
+++ uni/pages/device/index.vue
@@ -1,57 +1,20 @@
 <template>
-  <view class="device-page">
-    <web-view
-      src="http://192.168.0.71:8123"
-      :update-title="false"
-      :webview-styles="webviewStyles"
-    ></web-view>
-  </view>
+  <web-view
+    src="http://192.168.0.71:8123"
+    :update-title="false"
+  ></web-view>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      webviewStyles: {
-        progress: { color: '#007AFF' }
-      }
-    }
-  },
-  onReady() {
-    // #ifdef APP-PLUS
-    const info = uni.getSystemInfoSync()
-    const top = info.statusBarHeight + 44 // 状态栏 + 导航栏
-    const bottom = 50 // tabBar 高度
-    const currentWebview = this.$scope.$getAppWebview()
-    setTimeout(() => {
-      const wv = currentWebview.children()[0]
-      if (wv) {
-        wv.setStyle({ top, bottom })
-      }
-    }, 300)
-    // #endif
-
-    // #ifdef H5
-    setTimeout(() => {
-      const iframe = document.querySelector('.device-page iframe')
-      if (iframe) {
-        iframe.style.top = '44px'
-        iframe.style.height = 'calc(100vh - 44px - 50px)'
-      }
-    }, 300)
-    // #endif
-  },
   onShow() {
+    // #ifdef APP-PLUS
     uni.showTabBar()
+    // #endif
   }
 }
 </script>
 
 <style>
-.device-page {
-  width: 100%;
-  height: 100vh;
-  position: relative;
-  background: #fff;
-}
+/* web-view 自动占满，系统导航栏和tabBar为原生层覆盖在上 */
 </style>
