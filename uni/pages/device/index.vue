90e28858c9a72faceba29fd19b63066620a63288
Axiaobo <Axiaobo7788@outlook.com>
2026-04-30 16:57:23 +0800

    feat: update device page and pages.json 	modified:   uni/pages.json 	modified:   uni/pages/device/index.vue

--- uni/pages/device/index.vue
+++ uni/pages/device/index.vue
@@ -1,44 +1,48 @@
 <template>
   <view class="device-page">
-    <view class="nav-bar">
-      <text class="nav-title">智能家居</text>
-    </view>
     <web-view class="webview" src="http://192.168.0.71:8123"></web-view>
-    <custom-tabbar :current-index="1"></custom-tabbar>
+    <cover-view class="nav-bar">
+      <cover-view class="nav-title">智能家居</cover-view>
+    </cover-view>
   </view>
 </template>
 
 <script>
-export default {}
+export default {
+  onShow() {
+    uni.showTabBar()
+  }
+}
 </script>
 
-<style lang="scss">
+<style>
 .device-page {
   width: 100%;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-}
-
-.nav-bar {
-  height: 88rpx;
-  background: #fff;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-bottom: 1rpx solid #eee;
-  flex-shrink: 0;
-
-  .nav-title {
-    font-size: 32rpx;
-    font-weight: 600;
-    color: #333;
-  }
+  position: relative;
 }
 
 .webview {
-  flex: 1;
   width: 100%;
+  height: 100%;
+}
+
+.nav-bar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 44px;
+  background: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #e5e5e5;
+}
+
+.nav-title {
+  font-size: 17px;
+  font-weight: 600;
+  color: #333333;
 }
 </style>
